dddec2dbf67a295136f513b9fda5f8126c29ab9b
Y. Z. Chen <754097987@qq.com>
2019-03-28 22:05:06 +0800

    [Update]

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,7 @@ import java.net.URL
 
 object Main
 {
-    private fun getIssueAtPage(page: Int) = URL("https://api.github.com/repos/996icu/996.ICU/issues?filter=all&page=$page&access_token=${Token.ACCESS_TOKEN}").openConnection().getInputStream().bufferedReader().readText()
+    private fun getIssueAtPage(page: Int) = URL("https://api.github.com/repos/996icu/996.ICU/issues?state=all&filter=all&page=$page&access_token=${Token.ACCESS_TOKEN}").openConnection().getInputStream().bufferedReader().readText()
 
     private fun getAllIssue() =
         (1..650).map {
@@ -25,12 +25,13 @@ object Main
                 SummarizedIssue(obj["created_at"] as String, obj["number"] as Int, (obj["user"] as JSONObject)["login"] as String, (obj["user"] as JSONObject)["id"] as Int, obj["title"] as String, (obj["body"] as String).replace("\r", "").replace("\n", "\\n").replace("\t", "\\t") as String)
 //                "${obj["created_at"]}\t${obj["number"]}\t${(obj["user"] as JSONObject)["login"]}\t${(obj["user"] as JSONObject)["id"]}\t${obj["title"]}\t${(obj["body"] as String).replace("\r", "").replace("\n", "\\n").replace("\t", "\\t")}"
             }
-        }.flatten()
+        }.flatten().toHashSet()
 
     @JvmStatic
     fun main(args: Array<String>)
     {
-        println(analyseAllIssue().groupBy { it.user_login }.map { it.key to it.value.size }.sortedByDescending { it.second })
+//        getAllIssue()
+        analyseAllIssue().groupBy { it.user_login }.map { it.key to it.value.size }.sortedByDescending { it.second }.forEach { println("|${it.first}|${it.second}|") }
 //        val r = JSONArray.parseArray(File("./res/1").readText())[0] as JSONObject
 //        println(r.toJavaObject(Issue::class.java))
 //        File("./result.tsv").writeText(analyseAllIssue().joinToString(separator = "\n") { it })
